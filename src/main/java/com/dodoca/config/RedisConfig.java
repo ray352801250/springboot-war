3711b35e7455b2c3ef04f8745870020d13188ac6
tianguanghui <tianguanghui@dodoca.com>
2018-09-04 18:06:35 +0800

    初步整合redis

---
+++ src/main/java/com/dodoca/config/RedisConfig.java
@@ -0,0 +1,72 @@
+package com.dodoca.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+@Configuration
+@EnableAutoConfiguration
+public class RedisConfig {
+    private static Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+    @Value("${spring.redis.database}")
+    private  int database;
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.port}")
+    private  int port;
+
+    @Value("${spring.redis.timeout}")
+    private  int timeout;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis.jedis.pool")
+    public JedisPoolConfig jedisPoolConfig(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        return jedisPoolConfig;
+    }
+
+    @Bean
+//    @ConfigurationProperties(prefix = "spring.redis")
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setDatabase(database);
+//        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        redisStandaloneConfiguration.setPort(port);
+        //开启连接池
+        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder jedisPoolingClientConfigurationBuilder =
+                (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
+        jedisPoolingClientConfigurationBuilder.poolConfig(jedisPoolConfig());
+        JedisClientConfiguration jedisClientConfiguration = jedisPoolingClientConfigurationBuilder.build();
+        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration,
+                jedisClientConfiguration);
+        logger.info("JedisConnectionFactory bean init success.");
+        return factory;
+    }
+
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new StringRedisTemplate(jedisConnectionFactory());
+    }
+
+}
--- src/test/java/com/dodoca/DemoApplicationTests.java
+++ src/test/java/com/dodoca/DemoApplicationTests.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -16,12 +22,26 @@ public class DemoApplicationTests {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     public void contextLoads() throws SQLException {
 
-        Connection connection = dataSource.getConnection();
-        System.out.println(dataSource.getClass());
-        System.out.println(connection);
+        String result = stringRedisTemplate.execute(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                RedisSerializer<String> serializer = stringRedisTemplate.getStringSerializer();
+                byte[] value =  connection.get(serializer.serialize("aaa"));
+                return serializer.deserialize(value);
+            }
+        });
+        Object aaa1 = redisTemplate.opsForValue().get("aaa1");
+        System.out.println("aaa1: " + aaa1);
+        System.out.println("result: " + result);
     }
 
 }
